8cc6317779717a0ab534604f8e448b39ab3ae135
mtkachenko <oblomov86@gmail.com>
2018-04-06 14:03:54 +0300

    added test connection string and simple test migration

---
+++ src/DbMigrationTool/Migrations/1_CreateHumanTable.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using Dapper;
+
+namespace DbMigrationTool.Migrations
+{
+    public class CreateHumanTable : BaseMigration
+    {
+        protected override void RunMigration(IDbConnection conn, IDbTransaction tran)
+        {
+            conn.Execute(@"
+CREATE TABLE Human(
+    Id INT IDENTITY(1,1) NOT NULL,
+    Name NVARCHAR(100) NOT NULL,
+    BirthDay DATETIME NOT NULL,
+    CONSTRAINT PK_Id_Human PRIMARY KEY (Id))", null, tran);
+        }
+
+        protected override void RollbackMigration(IDbConnection conn, IDbTransaction tran)
+        {
+            conn.Execute("DROP TABLE Human", null, tran);
+        }
+    }
+}
